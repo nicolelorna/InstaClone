14d7928e3e36cffaeaa31c276b673772b8b9d46b
unknown <cynthianyaiga>
2020-10-26 03:12:54 +0300

    insta

--- app/src/main/java/com/example/instaclone/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/instaclone/ViewPagerAdapter.kt
@@ -1,4 +1,24 @@
 package com.example.instaclone
 
-class ViewPagerAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.instaclone.Fragments.*
+
+
+class ViewPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
+    override fun getCount(): Int {
+        return 5
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> HomeFragment()
+            1 -> SearchFragment()
+            2 -> AddPostFragment()
+            3 -> ActivityFragment()
+            4 -> ProfileFragment()
+            else -> HomeFragment()
+        }
+    }
 }
